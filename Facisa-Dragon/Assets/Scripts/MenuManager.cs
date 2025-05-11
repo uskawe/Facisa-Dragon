73f2f4417ae276bb88b41ac87ec9ff43e39ca592
uskawe <kaue.pereira.silva77@gmail.com>
2025-05-10 23:51:27 -0300

    Update 0.1

---
+++ Facisa-Dragon/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string sceneName;
+    public InputActionAsset inputActions; 
+    private InputAction anyKeyAction;
+
+    void Awake()
+    {
+        var menuMap = inputActions.FindActionMap("Menu");
+        anyKeyAction = menuMap.FindAction("AnyKey");
+
+        anyKeyAction.performed += ctx => ChangeScene();
+        anyKeyAction.Enable();
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    void OnDestroy()
+    {
+        anyKeyAction.Disable();
+    }
+}
