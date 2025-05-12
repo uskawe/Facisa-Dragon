931f4b66f52c41802e515963648ca59202947c1e
uskawe <kaue.pereira.silva77@gmail.com>
2025-05-12 16:21:05 -0300

    Update 0.2

--- Facisa-Dragon/Assets/Scripts/MenuManager.cs
+++ Facisa-Dragon/Assets/Scripts/MenuManager.cs
@@ -1,25 +1,91 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] private string sceneName;
-    public InputActionAsset inputActions; 
+    public InputActionAsset inputActions;
     private InputAction anyKeyAction;
+
+    [Header("UI Enable")]
+    [SerializeField] private GameObject mainMenu;
+
+    [Header("UI Disable")]
+    [SerializeField] private GameObject initialMenu;
+
+    [Header("Transition Settings")]
+    [SerializeField] private float transitionDuration = 1f;
+
+    private CanvasGroup mainMenuGroup;
+    private CanvasGroup initialMenuGroup;
 
     void Awake()
     {
         var menuMap = inputActions.FindActionMap("Menu");
         anyKeyAction = menuMap.FindAction("AnyKey");
 
-        anyKeyAction.performed += ctx => ChangeScene();
+        anyKeyAction.performed += ctx => ShowMenuUI();
         anyKeyAction.Enable();
+
+        mainMenuGroup = mainMenu.GetComponent<CanvasGroup>();
+        initialMenuGroup = initialMenu.GetComponent<CanvasGroup>();
+
+        mainMenu.SetActive(true);
+        mainMenuGroup.alpha = 0f;
+        mainMenuGroup.interactable = false;
+        mainMenuGroup.blocksRaycasts = false;
+
+        initialMenu.SetActive(true);
+        initialMenuGroup.alpha = 1f;
     }
 
-    void ChangeScene()
+    void ShowMenuUI()
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(FadeOutInitialThenFadeInMain());
+        anyKeyAction.Disable();
+    }
+
+    IEnumerator FadeOutInitialThenFadeInMain()
+    {
+        float time = 0f;
+
+        while (time < transitionDuration)
+        {
+            float t = time / transitionDuration;
+            if (initialMenuGroup != null)
+                initialMenuGroup.alpha = 1f - t;
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        if (initialMenuGroup != null)
+        {
+            initialMenuGroup.alpha = 0f;
+            initialMenuGroup.interactable = false;
+            initialMenuGroup.blocksRaycasts = false;
+            initialMenu.SetActive(false);
+        }
+
+        mainMenu.SetActive(true);
+        time = 0f;
+
+        while (time < transitionDuration)
+        {
+            float t = time / transitionDuration;
+            if (mainMenuGroup != null)
+                mainMenuGroup.alpha = t;
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        if (mainMenuGroup != null)
+        {
+            mainMenuGroup.alpha = 1f;
+            mainMenuGroup.interactable = true;
+            mainMenuGroup.blocksRaycasts = true;
+        }
     }
 
     void OnDestroy()
